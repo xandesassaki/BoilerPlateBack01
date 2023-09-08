e77148b4a193c64fbf50ca4e06d8ae4880a9b2de
Alexandre Sassaki <sassakixande@gmail.com>
2023-09-07 23:11:26 -0300

    Created base project with postgres driver and basic configurations of flyway and datasource using ENV

---
+++ src/test/java/com/sassaki/BoilerPlateVer01/BoilerPlateVer01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sassaki.BoilerPlateVer01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerPlateVer01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
